09a3e5c672e82f13c113d2da2687690de071d50b
Alexander Petros <apetros15@gmail.com>
2023-03-17 12:28:10 -0400

    Fix memory leak and update show simulations

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 // import fs from 'node:fs'
 import readline from 'node:readline/promises'
-import { exec, execSync } from 'node:child_process'
+import { execSync } from 'node:child_process'
 
 import Conversation from './conversation.js'
 import * as simulations from './simulations.js'
@@ -13,32 +13,47 @@ console.log(`
 Welcome to the ChatGPT TTS.
 Type your responses, or return an empty line and then speak into the microphone.`)
 
-const conversation = new Conversation(simulations.interrogation)
+let conversation = new Conversation(simulations.gameshow)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdin,
+  terminal: false
+})
+
 while (!conversation.isOver()) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdin,
-    terminal: false
-  })
-
-
   const line = await rl.question('')
   let userInput
   switch (line) {
     case '.dump':
       conversation.dumpHistoryToFile()
       break
-    case '':
+    case '.h':
+      conversation = new Conversation(simulations.gameshow)
+      break
+    case '.i':
+      conversation = new Conversation(simulations.interrogation)
+      break
+    case '': {
       userInput = await transcription.listen()
-      break
-    default:
+      if (userInput.toLowerCase().includes('interrogation')) {
+        conversation = new Conversation(simulations.interrogation)
+        execSync('say affirmative')
+        break
+      } else {
+        const response = await conversation.say(userInput)
+        execSync(`say "${response.replaceAll('"', '')}"`)
+        console.log(`> ${response}`)
+        break
+      }
+    }
+    default: {
       userInput = line
+      const response = await conversation.say(userInput)
+      execSync(`say "${response.replaceAll('"', '')}"`)
+      console.log(`> ${response}`)
       break
+    }
   }
-  const response = await conversation.say(userInput)
-
-  execSync(`say "${response.replaceAll('"', '')}"`)
-  console.log(`> ${response}`)
 }
 
 conversation.playEndAudio()
