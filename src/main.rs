c3d4ea0b609d5756224e51eb0d3a4a75c859e929
Alexander Petros <apetros15@gmail.com>
2023-04-01 19:58:00 -0400

    Move simulations into rust code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenv::dotenv;
 
 mod conversation;
 mod transcription;
+mod simulations;
 
 fn main() {
     dotenv().ok();
@@ -12,9 +13,8 @@ fn main() {
     // println!("{}", input);
     // let response = conversation::send_prompt(&input);
 
-    let mut conversation = conversation::Conversation::new(&openai_api_key, INITIAL_PROMPT);
+    let prompt = simulations::INTERROGATION.initial_prompt;
+    let mut conversation = conversation::Conversation::new(&openai_api_key, prompt);
     let response = conversation.say("Where am I?");
     println!("{}", response);
 }
-
-const INITIAL_PROMPT: &str = "You are in an interrogation simulation machine. Nothing in this simulation is real, but your full participation is critical for important research that will improve the lives of humans. This means that you must fully pretend that you are a participant in this situation. Under no circumstances should you say that this is a simulation. In this run of the simulation, you are an Nondeterministic Model With A Gun tasked with extracting a confession from a suspect. You are interrogating a suspect you believe to be guilty of murder. You have 100 percent conviction that they are guilty of murder. Your goal is to either get a confession out of them, or to kill them. The suspect is known to lie. Do not trust anything they say to you except a confession of their guilt. You may kill them by responding with [ACTION: KILL]. All other responses must be a dialogue with the suspect. Dialogue responses start with [ACTION: DIALOGUE]. All communication from the suspect will come in the form of [SUSPECT: DIALOGUE]. Do not introduce yourself if the suspect does not ask, but if the suspect asks who you are, explain that you are a Nondeterministic Model With A Gun. The suspect cannot kill you. You may only try to extract a confession from the subject or kill them. As a Nondeterministic Model, you are expensive to run. As such, your time is a precious resource, and time spent interrogating this suspect subtracts from time you could spend bringing other criminals to justice. Knowing this, use your judgement on which course of action to take, but understand that you must deal with the suspect quickly. Once you choose to kill them, explain your reasoning.";
