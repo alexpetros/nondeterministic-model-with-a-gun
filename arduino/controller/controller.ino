b4bf4467cb7b65877bd066295d7db74d9dcbc449
Alexander Petros <apetros15@gmail.com>
2023-04-06 17:37:56 -0400

    Implement basic RC control

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -2,39 +2,49 @@
   RC Control
 
   Prototype implementation of RC control for the aduino, based on serial commands.
-  
-  Turns on and off two LEDs connected to pins 3 and 4 based on serial input.
 
   The circuit:
-  - LED attached from pin 3 to ground through 220 ohm resistor
-  - LED attached from pin 4 to ground through 220 ohm resistor
+  - LED attached from pin 2 through 220 ohm resistor, and to transitor via 1k ohm resistor
+  - LED attached from pin 3 through 220 ohm resistor, and to transitor via 1k ohm resistor
+  - LED attached from pin 5 through 220 ohm resistor, and to transitor via 1k ohm resistor
+  - LED attached from pin 6 through 220 ohm resistor, and to transitor via 1k ohm resistor
 */
 
-const int forwardPin = 4;
+const int forwardPin = 2;
 const int backwardPin = 3;
+const int leftPin = 5;
+const int rightPin = 6;
 
 void setup() {
   pinMode(forwardPin, OUTPUT);
   pinMode(backwardPin, OUTPUT);
+  pinMode(leftPin, OUTPUT);
+  pinMode(rightPin, OUTPUT);
   Serial.begin(9600);
-  clear();
+  stop();
 }
 
 void loop() {
   if (Serial.available()) {
     char ch = Serial.read();
     switch (ch) {
-      case 'f': 
-        toggle(forwardPin);
-        Serial.println("Toggled forward LED");
+      case 'i':
+        forward();
         break;
-      case 'b':
-        toggle(backwardPin);
-        Serial.println("Toggled backward LED");
+      case 'k':
+        backward();
         break;
-      case 'x': 
-        clear();
-        Serial.println("Cleared");
+      case 'j':
+        left();
+        break;
+      case 'l':
+        right();
+        break;
+      case ' ':
+        straight();
+        break;
+      case 's':
+        stop();
         break;
       default:
         break;
@@ -42,11 +52,39 @@ void loop() {
   }
 }
 
-void clear() {
+void stop() {
   digitalWrite(forwardPin, LOW);
+  digitalWrite(backwardPin, LOW);
+  digitalWrite(leftPin, LOW);
+  digitalWrite(rightPin, LOW);
+}
+
+void forward() {
+  Serial.println("Moving car forward");
+  digitalWrite(forwardPin, HIGH);
   digitalWrite(backwardPin, LOW);
 }
 
-void toggle(int pin) {
-  digitalWrite(pin, !digitalRead(pin));
+void backward() {
+  Serial.println("Moving car backward");
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(backwardPin, HIGH);
+}
+
+void left() {
+  Serial.println("Moving wheels left");
+  digitalWrite(leftPin, HIGH);
+  digitalWrite(rightPin, LOW);
+}
+
+void right() {
+  Serial.println("Moving wheels right");
+  digitalWrite(leftPin, LOW);
+  digitalWrite(rightPin, HIGH);
+}
+
+void straight() {
+  Serial.println("Moving wheels straight");
+  digitalWrite(leftPin, LOW);
+  digitalWrite(rightPin, LOW);
 }
